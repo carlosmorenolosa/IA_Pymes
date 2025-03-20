1f2db0fb1371eb48e5a929c2652506f92bcdc71f
carlosmorenolosa <carlosmorenolosa2001@gmail.com>
2025-03-20 09:33:26 +0100

    cambios en el diseño

--- src/App.js
+++ src/App.js
@@ -80,9 +80,10 @@ function HomeSection() {
   return (
     <section
       id="home"
-      className="relative flex items-center justify-center bg-gradient-to-r from-blue-100 to-indigo-300 text-gray-900 py-24 px-4"
+      className="relative flex items-center justify-center bg-gradient-to-b from-blue-100 to-indigo-300 text-gray-900 py-24 px-4"
       style={{ minHeight: "80vh" }}
     >
+
 
 
       <motion.div
